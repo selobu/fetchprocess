aefd1c3ed9ec38fc5d017bd091f0a9846b70b891
Sebastian Lopez Buritica <selobu@gmail.com>
2023-11-16 15:10:26 -0500

    style: :lipstick: lint code

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
+""" Configuration file
+"""
 __all__= ['Config']
 
 from os import environ
 appname = environ.get("APPNAME", default='fetchprocess')
 
-class Config(object):
+class Config:
     API_NAME: str = "QRSchool api"
     VERSION: str = "0.0.2"
     API_URL_PREFIX: str = "/api"
@@ -16,11 +18,8 @@ class Config(object):
         "url": "https://lteam.gestionhseq.com",
         "telegram": "https://t.me/selopez",
     }
-    JWT_SECRET_KEY: str = jwt_key
-    WTF_CSRF_SECRET_KEY: str = jwt_key * 2
     app: object = {}
     engine: object = {}
-    ECHO: bool = echo_value  # type:ignore
     APP_NAME: str = appname  # type:ignore
     FLASK_ADMIN_SWATCH: str = "cerulean"  # admin bootswatch theme
     ADMIN_TEMPLATE_NAME: str = "bootstrap4"
